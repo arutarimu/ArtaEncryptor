b7e441b526aafbe9cd859c2bbfa74e8e2a272391
Alex Kim <disy455@gmail.com>
2018-04-03 02:17:37 -0400

    Implemented Key Padding

--- ArtaEncryptor.py
+++ ArtaEncryptor.py
@@ -1,10 +1,11 @@
+
 # Made by Arta (Alex Kim)
 # This code is to encrypt a word using a key with the same amount of letters
 # It uses alphabetical indices to determine what letter to encrypt to
 
 # Using an alphabet list to generate a list of letters to encrypt from
 alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-                   "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 FINAL_LIST = []
 for x in range(0, len(alphabet_list)):
     alphabet_list.append(alphabet_list[x].upper())
@@ -12,10 +13,8 @@ for y in range(0, int(len(alphabet_list)/2)):
     FINAL_LIST.append(alphabet_list[y])
     FINAL_LIST.append(alphabet_list[len(alphabet_list) - y - 1])
 
-# Creating the class
 
-
-class ArtaEncryptor:
+class ArtaEncryptor: # Creating the class
 
     def __init__(self, word="", key=""):
         self.word = word
@@ -38,26 +37,33 @@ class ArtaEncryptor:
             if letter == FINAL_LIST[i]:
                 return i
 
+    def pad_key(self):  # Padding the key to match the length of word
+        if len(self.word) > len(self.key):
+            index_limit = len(self.key)
+            for i in range(len(self.key), len(self.word)):
+                self.key += self.key[i-index_limit]
+        else:
+            new_key = ""
+            for i in range(0, len(self.word)):
+                new_key += self.key[i]
+            self.set_key(new_key)
+
     def encrypt(self):  # Encryption is done by adding the letter at i position for both word and key
                         # Uses i as index to find the corresponding letter in FINAL_LIST
         encrypted = ""
-        if len(self.get_word()) == len(self.get_key()):
-            for i in range(0, len(self.get_word())):
-                encrypted_letter = FINAL_LIST[(self.find_index(self.get_word()[i]) + self.find_index(self.get_key()[i])) % 52]
-                encrypted += encrypted_letter
-            return encrypted
-        else:
-            return "Invalid length of key"
+        self.pad_key()
+        for i in range(0, len(self.get_word())):
+            encrypted_letter = FINAL_LIST[(self.find_index(self.get_word()[i]) + self.find_index(self.get_key()[i])) % 52]
+            encrypted += encrypted_letter
+        return encrypted
 
     def decrypt(self):  # Decryption is done by the same mechanism, but reversed from encrypt()
         decrypted = ""
-        if len(self.get_word()) == len(self.get_key()):
-            for i in range(0, len(self.get_word())):
-                decrypted_letter = FINAL_LIST[self.find_index(self.get_word()[i]) - self.find_index(self.get_key()[i])]
-                decrypted += decrypted_letter
-            return decrypted
-        else:
-            return "Invalid length of key"
+        self.pad_key()
+        for i in range(0, len(self.get_word())):
+            decrypted_letter = FINAL_LIST[self.find_index(self.get_word()[i]) - self.find_index(self.get_key()[i])]
+            decrypted += decrypted_letter
+        return decrypted
 
 
 def main():
